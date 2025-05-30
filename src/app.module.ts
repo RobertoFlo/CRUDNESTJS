ccc5f2a4a5503e3e52c47c2d43de31f92b8796c4
RFlore5 <raflores@innovacion.gob.sv>
2025-05-30 13:47:15 -0600

    version 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { JoiValidationSchema } from './config/joi.validation';
       rootPath: join(__dirname,'..','public'),
       }),
 
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB,{dbName:'pokemon'}),
     CommonModule,
     SeedModule,
   ],
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,7 +2,7 @@ import * as Joi from "joi";
 
 
 export const JoiValidationSchema = Joi.object({
-   PORT: Joi.number().default(3010),
+   PORT: Joi.number().default(3009),
    MONGODB: Joi.string().required(),
    DEFAULT_LIMIT: Joi.number().default(10),
 });
